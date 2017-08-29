8ce30e24cdbe11910dffa23d87f4ad34c80350f2
Jakub Roman <jakub@jakubroman.sk>
2017-08-29 21:56:02 +0200

    backup

--- backup.ps1
+++ backup.ps1
@@ -1,6 +1,6 @@
-try { 
+try {
   Remove-Item D:\archive\* -Include "*.rar";
 } catch { }
 
 $timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ");
-& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md512m -htb-oi:131072 -qo- -- "D:\archive\src_$timestamp.rar" "C:\dev\src";
+& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- -- "D:\archive\src_$timestamp.rar" "C:\dev\src";
