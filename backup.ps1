276b482c16f62f077f72c1d55384505467632b45
Jakub Roman <jakubro@users.noreply.github.com>
2017-09-27 10:29:31 +0200

    Update backup.ps1

--- backup.ps1
+++ backup.ps1
@@ -9,4 +9,16 @@ $timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ")
 $source = "C:\dev\src"
 $destination = "D:\archive\src_$timestamp.rar"
 
-& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- $destination $source
+# WinRar arguments explanation:
+#     a                 create archive
+#     -t                test files after archiving
+#     -k                lock archive
+#     -ibck             run in background
+#     -ma5              
+#     -m5               use best compression method
+#     -md1024m          use dictionary of size 1024MB
+#     -htb-oi:131072    
+#     -qo-              
+#     --                stop switches scanning
+
+& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- -- $destination $source
