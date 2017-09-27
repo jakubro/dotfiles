76ff53987a8eb391e6ea5c1dc3c92c37f8ae23dd
Jakub Roman <jakubro@users.noreply.github.com>
2017-09-27 10:19:54 +0200

    Update backup.ps1

--- backup.ps1
+++ backup.ps1
@@ -1,8 +1,12 @@
 Set-StrictMode -Version 2.0
 
 try {
-  Remove-Item D:\archive\* -Include "*.rar";
+  Remove-Item D:\archive\* -Include "*.rar"
 } catch { }
 
-$timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ");
-& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- "D:\archive\src_$timestamp.rar" "C:\dev\src"
+$timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ")
+
+$source = "C:\dev\src"
+$destination = "D:\archive\src_$timestamp.rar"
+
+& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- $destination $source
