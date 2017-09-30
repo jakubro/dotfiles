5e832f75a3aa91d8e7e3ca71e091ff3f6d47f32d
Jakub Roman <jakub@jakubroman.sk>
2017-09-30 19:30:25 +0200

    move backup configuration to top

--- backup.ps1
+++ backup.ps1
@@ -1,25 +1,25 @@
 Set-StrictMode -Version 2.0
 
+# what to backup & where to save the backup
+$timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ")
+$source = "C:\dev\src"
+$destination = "D:\archive\src_$timestamp.rar"
+
 # clean old backups
 try {
   Remove-Item D:\archive\* -Include "*.rar"
 } catch { }
-
-# create new backup
-$timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ")
-$source = "C:\dev\src"
-$destination = "D:\archive\src_$timestamp.rar"
 
 # WinRar arguments explanation:
 #     a                 create archive
 #     -t                test files after archiving
 #     -k                lock archive
 #     -ibck             run in background
-#     -ma5              
+#     -ma5
 #     -m5               use best compression method
 #     -md1024m          use dictionary of size 1024MB
-#     -htb-oi:131072    
-#     -qo-              
+#     -htb-oi:131072
+#     -qo-
 #     --                stop switches scanning
 
 & "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- -- $destination $source
