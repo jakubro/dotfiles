a385721696966f6835b558aa03f0103f503edc0b
Jakub Roman <jakub@jakubroman.sk>
2017-12-17 17:41:42 +0100

    add time and name to prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,7 +19,9 @@ Set-StrictMode -Version 2.0
 }
 
 # check changes in %PATH%
-Checkpoint-EnvironmentVariable -Name "PATH" -File "~\.path.txt"
+if (-not $env:SupressEnvCheckpoint) {
+  Checkpoint-EnvironmentVariable -Name "PATH" -File "~\.path.txt"
+}
 
 # chocolatey
 & {
@@ -29,10 +31,32 @@ Checkpoint-EnvironmentVariable -Name "PATH" -File "~\.path.txt"
   }
 }
 
+# prompt
+function Prompt {
+  $prefix = ""
+  if (-not [string]::IsNullOrWhiteSpace($env:ParentPromptName)) {
+    $name = $env:ParentPromptName.Trim()
+    $prefix = "($name) "
+  }
+  "PS " + $prefix + (Get-Date -Format "HH:mm") + " " + (Get-Location) + ">"
+}
+
+# conda
+function Set-CondaEnvironment {
+  cmd /C (
+    "C:\ProgramData\Anaconda3\Scripts\activate $args && " +
+    "set SupressEnvCheckpoint=true && " +
+    "set ParentPromptName=$args && " +
+    "powershell -NoExit -NoLogo"
+  )
+}
+
+Set-Alias conda "C:\ProgramData\Anaconda3\Scripts\conda.exe"
+Set-Alias conda-activate Set-CondaEnvironment
+
 # aliases
 Set-Alias codecompare "C:\Program Files\\Devart\Code Compare\CodeCompare.exe"
 Set-Alias codemerge "C:\Program Files\\Devart\Code Compare\CodeMerge.exe"
-Set-Alias conda "C:\ProgramData\Anaconda3\Scripts\conda.exe"
 Set-Alias de4dot "C:\tools\de4dot\de4dot-x64.exe"
 Set-Alias devenv "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"
 Set-Alias openssl "C:\tools\OpenSSL-Win32\bin\openssl.exe"
