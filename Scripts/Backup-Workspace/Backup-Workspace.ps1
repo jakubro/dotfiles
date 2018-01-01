4fcf069f3d53a69127394137f6e2e449e24a6124
Jakub Roman <jakub@jakubroman.sk>
2018-01-01 18:36:29 +0100

    load backup paths from workspace

--- Scripts/Backup-Workspace/Backup-Workspace.ps1
+++ Scripts/Backup-Workspace/Backup-Workspace.ps1
@@ -1,21 +1,41 @@
+#Requires -Modules Import-PSWorkspace
 Set-StrictMode -Version 2.0
+
+# load backup configuration from workspace (this script is running
+# in "-NoProfile" environment, so we have to load this manually)
+
+$PSWorkspace = Import-PSWorkspace -Path "~\.env" -Initial @{
+  BACKUP_SOURCE = $null;
+  BACKUP_DESTINATION = $null;
+}
 
 # what to backup & where to save the backup
 
 $timestamp = [DateTime]::UtcNow.ToString("yyyyMMddHHmmssZ")
-$source = "C:\dev\src"
-$destination = "G:\Team Drives\Backup\src_$timestamp.rar"
+
+if ([string]::IsNullOrWhiteSpace($PSWorkspace.BACKUP_SOURCE) -or
+   [string]::IsNullOrWhiteSpace($PSWorkspace.BACKUP_DESTINATION)) {
+  # todo: add error to event log
+  return
+}
+
+$source = $PSWorkspace.BACKUP_SOURCE.Trim("\","/") # trailing slash causes arguments to be interpreted incorrectly
+$destination = Join-Path $PSWorkspace.BACKUP_DESTINATION "backup_$timestamp.rar"
+
+# todo: add infor to event log
 
 # WinRar arguments explanation:
 #     a                 create archive
 #     -t                test files after archiving
 #     -k                lock archive
 #     -ibck             run in background
-#     -ma5
+#     -ma5              use RAR5.0 archive format
 #     -m5               use best compression method
 #     -md1024m          use dictionary of size 1024MB
-#     -htb-oi:131072
-#     -qo-
+#     -htb              use BLAKE2 hash
+#     -oi:131072        save identical files larger than 128KB as references
+#     -qo-              don't add quick open information
 #     --                stop switches scanning
 
-& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb-oi:131072 -qo- -- $destination $source
+Write-Host $destination $source
+& "${env:ProgramFiles}\WinRAR\WinRAR.exe" a -t -k -ibck -ma5 -m5 -md1024m -htb -oi:131072 -qo- -- $destination $source
