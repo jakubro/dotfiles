84da16580393ba6e4ccf8b2e840001ef6bd7d06c
Jakub Roman <jakub@jakubroman.sk>
2018-01-08 08:13:44 +0100

    fix creating event logs

--- Modules/TryCreate-EventLog/TryCreate-EventLog.psm1
+++ Modules/TryCreate-EventLog/TryCreate-EventLog.psm1
@@ -7,8 +7,25 @@ function TryCreate-EventLog {
     [Parameter(Mandatory=$true)]
     [string[]] $Source
   )
+
+  $register = @()
+  foreach ($sourceName in $Source) {
+    # register this source if it does not exist yet
+    if (![System.Diagnostics.EventLog]::SourceExists($sourceName)) {
+      $register += $sourceName
+      continue
+    }
+
+    # abort if the source is already registered with another log
+    $logFromSource = [System.Diagnostics.EventLog]::LogNameFromSourceName($sourceName, ".")
+    if (!($logFromSource -eq $LogName)) {
+      throw "The '$sourceName' source is already registered to '$logFromSource' log."
+    }
+  }
+
+  # create log and register provided sources
   if (![System.Diagnostics.EventLog]::Exists($LogName) -or
-      ![System.Diagnostics.EventLog]::SourceExists($Source)) {
-    New-EventLog -LogName $LogName -Source $Source
+      $register.Length -gt 0) {
+    New-EventLog -LogName $LogName -Source $register
   }
 }
