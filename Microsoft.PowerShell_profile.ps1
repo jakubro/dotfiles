c3cc626d6eed81df231136b35a1de5f4c72b69ca
Jakub Roman <jakub@jakubroman.sk>
2018-09-08 02:11:18 +0200

    next

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -24,6 +24,8 @@ $ProfileDirectory = Split-Path $PROFILE
 Import-Module "$ProfileDirectory\prompt.psm1"
 Import-Module "$ProfileDirectory\aliases.psm1"
 
+# python
+$env:Path= "$env:APPDATA\Python\Python36\site-packages\;$env:Path"
 
 # Third-party Modules
 ###############################################################################
