1d92c1c70e370f000a09d2d5dcaab970ac46fb5e
Jakub Roman <jakub@jakubroman.sk>
2019-03-25 14:59:38 +0100

    fix - length of string was capped to len of the alphabet

--- Modules/Get-RandomString/Get-RandomString.psm1
+++ Modules/Get-RandomString/Get-RandomString.psm1
@@ -1,6 +1,10 @@
 Set-StrictMode -Version 2.0
 
 function Get-RandomString($Length) {
+  $rv = ""
   # all printable ascii characters: from char 33 (!) to char 126 (~)
-  return -join ((33..126) | Get-Random -Count $Length | % { [char]$_ })
+  do {
+    $rv += -join ((33..126) | Get-Random -Count $Length | % { [char]$_ })
+  } while($rv.Length -le $Length)
+  return $rv
 }
