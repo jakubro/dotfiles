970b2058fac5084ef172e037a52474d165ab33a2
Jakub Roman <jakub@jakubroman.sk>
2019-06-17 14:52:26 +0200

    create new venv if not set yet

--- Modules/Invoke-PipInstall/Invoke-PipInstall.psm1
+++ Modules/Invoke-PipInstall/Invoke-PipInstall.psm1
@@ -1,6 +1,23 @@
 Set-StrictMode -Version 2.0
 
 function Invoke-PipInstall {
+  if (!$env:VirtualEnvRoot -or !(Test-Path (Join-Path $env:VirtualEnvRoot '.venv\Scripts\Activate.ps1'))) {
+    Write-Host -ForegroundColor Yellow "Virtual environment does not exist. Creating new one ..."
+    python -m venv .venv
+  } else {
+    Write-Host -ForegroundColor Yellow "Using virtual environment from $env:VirtualEnvRoot ..."
+  }
+
+  Enter-PythonEnvironment
+
+  if (!(which python).Source.StartsWith((Join-Path $env:VirtualEnvRoot '.venv')) -or
+      !(which pip).Source.StartsWith((Join-Path $env:VirtualEnvRoot '.venv'))) {
+    throw "python.exe or pip.exe does not point to current virtual environment."
+  }
+
+  Write-Host -ForegroundColor Yellow "Installing $args ..."
   iex "pip install $args"
-  pip freeze | Out-File -Encoding ascii .\requirements.lock.txt
+
+  Write-Host -ForegroundColor Yellow "Installed $args. Updating requirements.lock.txt ..."
+  pip freeze | Out-File -Encoding ascii (Join-Path $env:VirtualEnvRoot 'requirements.lock.txt')
 }
