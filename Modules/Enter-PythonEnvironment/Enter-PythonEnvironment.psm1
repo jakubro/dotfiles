fbb68c40ec18dc6eafd22a5f4d52f37b2b22d4b3
Jakub Roman <jakub@jakubroman.sk>
2019-06-17 13:57:24 +0200

    automatic venv switching when cd to subdir

--- Modules/Enter-PythonEnvironment/Enter-PythonEnvironment.psm1
+++ Modules/Enter-PythonEnvironment/Enter-PythonEnvironment.psm1
@@ -4,22 +4,41 @@ function Enter-PythonEnvironment {
   # Automatically (de/)activate Python virtual environment (venv),
   # depending on the current location.
 
+  # deactivate
   if ($env:VirtualEnvRoot) {
-    # deactivate
-    if (!$PWD.Path.StartsWith($env:VirtualEnvRoot)) {
-      deactivate
-      $env:VirtualEnvRoot = $null
-      $env:ParentPSPromptName = $null
-    }
+    Deactivate-PythonEnvironment $PWD.Path
   }
 
+  # activate
   if (!$env:VirtualEnvRoot) {
-    # activate
-    if (Test-Path .\.venv\Scripts\Activate.ps1) {
-      $env:VirtualEnvRoot = $PWD.Path
-      $env:VIRTUAL_ENV_DISABLE_PROMPT = $true
-      $env:ParentPSPromptName = Split-Path -Leaf $PWD.ProviderPath
-      .\.venv\Scripts\Activate.ps1
+    $path = $PWD.Path
+    while ($path) {
+      if (Activate-PythonEnvironment $path) {
+        break
+      }
+      $path = Split-Path -Parent $path
     }
   }
 }
+
+function Deactivate-PythonEnvironment($Path) {
+  if (!$Path.StartsWith($env:VirtualEnvRoot)) {
+    deactivate
+    $env:VirtualEnvRoot = $null
+    $env:ParentPSPromptName = $null
+  }
+}
+
+function Activate-PythonEnvironment($Path) {
+  $activate = Join-Path $Path '.venv\Scripts\Activate.ps1'
+  if (Test-Path $activate) {
+    $env:VirtualEnvRoot = $Path
+    $env:VIRTUAL_ENV_DISABLE_PROMPT = $true
+    $env:ParentPSPromptName = Split-Path -Leaf $Path
+    & $activate
+    return $true
+  }
+  return $false
+}
+
+Export-ModuleMember -Function Enter-PythonEnvironment
